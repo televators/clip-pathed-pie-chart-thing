17842c6579ce72f8622a884dbabc97a57ba92e38
Justin Higgs <justinphiggs@gmail.com>
2022-05-27 00:58:30 -0400

    Did a pretty big overhaul of the sizing, as well as removed vestigial markup and classes. Now closer to being fully responsive. It's responsive enough for the use case here. Created a folder for final minified markup for each instance on the site.

--- js/index.js
+++ js/index.js
@@ -5,12 +5,16 @@
 
   const slices = document.querySelectorAll( '.wheel__slice' );
 
+  if ( ! slices.length > 0 ) {
+    return;
+  }
+
   function toggle_slice_and_phase( event ) {
-    const prev_slice       = document.querySelector( '.wheel__slice.--active' ),
-      active_slice         = event.target.closest( '.wheel__slice' ),
-      prev_content         = document.querySelector( '.start-phase.--active' ),
-      slice_content_id     = active_slice.getAttribute( 'data-content-id' ),
-      active_content       = document.querySelector( `#${ slice_content_id }` );
+    const prev_slice   = document.querySelector( '.wheel__slice.--active' ),
+      active_slice     = event.target.closest( '.wheel__slice' ),
+      prev_content     = document.querySelector( '.start-phase.--active' ),
+      slice_content_id = active_slice.getAttribute( 'data-content-id' ),
+      active_content   = document.querySelector( `#${ slice_content_id }` );
 
     // Cancel if user clicks already active slice
     if ( prev_slice == active_slice ) {
